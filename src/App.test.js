28fc70bc670e14b22635aa7dfe97b9e03ec3bcb2
Filipe Oliveira <hi@fmoliveira.dev>
2021-06-29 20:28:17 -0300

    Remove boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const heading = screen.getByRole(
-    'heading',
-    /Side React Take-home Assignment/i,
-  );
-  expect(heading).toBeInTheDocument();
+  expect(() => render(<App />)).not.toThrow();
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import './bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>

20b8b4d40e890bb8cea0a01330207329cd13df04
Filipe Oliveira <hi@fmoliveira.dev>
2021-06-29 22:05:13 -0300

    Fetch properties from api

---
+++ src/api/simplyRetsApi.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+
+const BASE_URL = 'https://api.simplyrets.com';
+const LOGIN = process.env.REACT_APP_SIMPLYRETS_LOGIN;
+const PASSWORD = process.env.REACT_APP_SIMPLYRETS_PASSWORD;
+
+const LISTINGS_CACHE_KEY = 'cache:listings';
+
+// try to get cached listings from local storage
+const getCachedListings = () => {
+  try {
+    const cachedItem = localStorage.getItem(LISTINGS_CACHE_KEY);
+    if (cachedItem) {
+      const deserializedData = JSON.parse(cachedItem);
+      if (Array.isArray(deserializedData)) {
+        return deserializedData;
+      }
+    }
+  } catch (error) {
+    // fail silently because caching is not critical
+  }
+  return undefined;
+};
+
+// keep listings in local storage
+const setCachedListings = (data) => {
+  try {
+    localStorage.setItem(LISTINGS_CACHE_KEY, JSON.stringify(data));
+  } catch (error) {
+    // fail silently because caching is not critical
+  }
+};
+
+// prepare common authorization headers for reuse
+const BASIC_CREDENTIALS = btoa(`${LOGIN}:${PASSWORD}`);
+const Authorization = `Basic ${BASIC_CREDENTIALS}`;
+const headers = new Headers({ Authorization });
+
+/**
+ * Get property listings.
+ *
+ * @returns { isLoading, data, error }  Object with request status and returned data
+ */
+export function usePropertyListings() {
+  const [data, setData] = useState(() => getCachedListings() ?? []);
+  const [status, setStatus] = useState('pending');
+
+  // fetch listings from cache-first and then the api
+  const fetchPropertyListings = async () => {
+    // try to get from the local storage cache
+    try {
+      const data = getCachedListings();
+      if (data) {
+        setStatus('success');
+        setData(data);
+        return;
+      }
+    } catch (error) {
+      // fail silently and proceed to fetching from the api
+    }
+
+    // try to fetch from the api
+    try {
+      const data = await fetch(`${BASE_URL}/properties`, { headers }).then(
+        (res) => res.json(),
+      );
+      setData(data);
+      setStatus('success');
+      setCachedListings(data);
+    } catch (error) {
+      setStatus('error');
+    }
+  };
+
+  // fetch listings when initializing hook
+  useEffect(() => fetchPropertyListings(), []);
+
+  const isLoading = status === 'loading';
+  const error = status === 'error';
+
+  return { isLoading, data, error };
+}
--- src/pages/PropertyListings.js
+++ src/pages/PropertyListings.js
@@ -1,9 +1,14 @@
+import { usePropertyListings } from '../api/simplyRetsApi';
+
 import Header from '../components/Header';
 
 export default function PropertyListings() {
+  const { isLoading, data, error } = usePropertyListings();
+
   return (
     <div>
       <Header>Property Listings</Header>
+      <p>Listing {data.length} properties</p>
     </div>
   );
 }

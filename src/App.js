12b0579ad270a565abd20da7958bf40260a56644
Filipe Oliveira <hi@fmoliveira.dev>
2021-06-29 21:19:29 -0300

    Setup theming

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Suspense } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 
 import Loader from './components/Loader';
 
 import Routes from './pages/_routes';
+import theme from './styles/theme';
 
 export default function App() {
   return (
     <Suspense fallback={<Loader />}>
       <Normalize />
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </Suspense>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  colors: {
+    headerBackground: '#f4f4f4',
+  },
+};
+
+export default theme;
